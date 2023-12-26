a4486002cb416d0b3089373ab2b6ef2849826d1f
vineethsankre <lsvineethsankre@gmail.com>
2023-12-26 12:26:47 +0000

    Added RestaurantController for implementing APIs

--- src/main/java/com/example/dinemaster/controller/RestaurantController.java
+++ src/main/java/com/example/dinemaster/controller/RestaurantController.java
@@ -1,11 +1,40 @@
-/*
- *
- * You can use the following import statements
- * 
- * import org.springframework.beans.factory.annotation.Autowired;
- * import org.springframework.web.bind.annotation.*;
- * import java.util.ArrayList;
- * 
- */
+package com.example.dinemaster.controller;
 
-// Write your code here
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.ArrayList;
+
+import com.example.dinemaster.model.*;
+import com.example.dinemaster.service.RestaurantJpaService;
+
+@RestController
+public class RestaurantController {
+
+    @Autowired
+    private RestaurantJpaService restaurantJpaService;
+
+    @GetMapping("/restaurants")
+    public ArrayList<Restaurant> getRestaurants() {
+        return restaurantJpaService.getRestaurants();
+    }
+
+    @PostMapping("/restaurants")
+    public Restaurant addRestaurant(@RequestBody Restaurant restaurant) {
+        return restaurantJpaService.addRestaurant(restaurant);
+    }
+
+    @GetMapping("/restaurants/{restaurantId}")
+    public Restaurant getRestaurantById(@PathVariable("restaurantId") int id) {
+        return restaurantJpaService.getRestaurantById(id);
+    }
+
+    @PutMapping("/restaurants/{restaurantId}")
+    public Restaurant updateRestaurant(@PathVariable("restaurantId") int id, @RequestBody Restaurant restaurant) {
+        return restaurantJpaService.updateRestaurant(id, restaurant);
+    }
+
+    @DeleteMapping("/restaurants/{restaurantId}")
+    public void deleteRestaurant(@PathVariable("restaurantId") int id) {
+        restaurantJpaService.deleteRestaurant(id);
+    }
+}
